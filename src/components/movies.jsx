d6c12a5e1221e296b736ea80cb645ebe1e105ad5
Matthew Daines <matthew@1from2.com>
2020-05-12 13:28:54 -0600

    Handle movie delete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import { remove } from 'lodash';
 
 class Movies extends Component {
 	state = {
@@ -7,28 +8,36 @@ class Movies extends Component {
 	}
 
 	handleDelete = movie => {
-		console.log(movie);
+		remove(this.state.movies, (thisMovie) => { return thisMovie._id === movie._id; });
+		this.setState({ movies: this.state.movies });
 	};
 
 	render() {
 		return (
-			<main className="container">
+			<div className="container">
 				<p>Showing {this.state.movies.length} movies in the database.</p>
-				<div>{ this.state.movies.map(movie => this.renderLineItem(movie) ) }</div>
-			</main>
+				<div>
+					<div className="row">
+						<div className="col-md">Title</div>
+						<div className="col-md">Genre</div>
+						<div className="col-md">Stock</div>
+						<div className="col-md">Rate</div>
+						<div className="col-md">&nbsp;</div>
+					</div>
+					{ this.state.movies.map(movie => this.renderLineItem(movie) ) }
+				</div>
+			</div>
 		);
 	}
 
 	renderLineItem(movie) {
-		console.log(movie);
-		return <div key={movie._id}>
-			<div>{movie.title}</div>
-			<div>{movie.genre.name }</div>
-			<div>{movie.numberInStock }</div>
-			<div>{movie.dailyRentalRate }</div>
-			<div><button onClick={ this.handleDelete(movie) } className="btn btn-secondary btn-sm">Delete</button></div>
+		return <div key={movie._id} className="row">
+			<div className="col-sm">{movie.title}</div>
+			<div className="col-sm">{movie.genre.name }</div>
+			<div className="col-sm">{movie.numberInStock }</div>
+			<div className="col-sm">{movie.dailyRentalRate }</div>
+			<div className="col-sm"><button onClick={ () => this.handleDelete(movie) } className="btn btn-secondary btn-sm">Delete</button></div>
 		</div>
-		// return <div key={movie._id}><div>{ movie.title }</div><div>{ movie.genre }</div><div>{ movie.numberInStock }</div><div>{ movie.dailyRentalRate }</div></div>
 	}
 }
 
