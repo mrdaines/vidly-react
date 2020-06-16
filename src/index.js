7f19c6847306ae692a5f30abcd9cb0e4802a0644
Matthew Daines <matthew@1from2.com>
2020-06-16 11:23:26 -0600

    Routing exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Movies from './components/movies';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
 ReactDOM.render(
-  <Movies />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
