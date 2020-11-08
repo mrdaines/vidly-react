6742d490c88894768530bb2ea51d46591e91f567
Matthew Daines <matthew@1from2.com>
2020-11-08 14:49:47 -0700

    Forms14 - Validating a Form Using JOI

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,30 +9,23 @@ class LoginForm extends Component {
 	};
 
 	schema = {
-		username: Joi.string().required(),
-		password: Joi.string().required().min(8)
+		username: Joi.string().required().label("Username"),
+		password: Joi.string().required().min(8).label("Password")
 	}
 
 	validate = () => {
-		const result = Joi.validate(
-			this.state.account,
-			this.schema,
-			{ abortEarly: false }
-		);
-		console.log(result);
+		const options = { abortEarly: false };
+		const { error } = Joi.validate(this.state.account, this.schema, options);
+		if (!error) return null;
 
 		const errors = {};
-
-		const { account } = this.state;
-		if (account.username.trim() === '')
-			errors.username = 'Username is required.';
-
-		if (account.password.trim() === '')
-			errors.password = 'Password is required.';
-		else if (account.password.trim().length < 8)
-			errors.password = 'Password must be 8 characters or longer.';
-
-		return Object.keys(errors).length === 0 ? null : errors;
+		for (let item of error.details) {
+			errors[item.path[0]] = errors[item.path[0]] ?
+				errors[item.path[0]] + " AND " + item.message :
+				item.message;
+		}
+		console.log(errors);
+		return errors;
 	}
 
 	handleSubmit = e => {
